a3d0eed7e95301d85389dad7bba941ad09ae5966
Sal Gutierrez <sgutie39@calpoly.edu>
2022-12-04 14:23:29 -0800

    Added TODO

--- src/main/scala/lyricgen/LyricBags.scala
+++ src/main/scala/lyricgen/LyricBags.scala
@@ -8,7 +8,7 @@ object LyricBags {
   def main (args : Array[String]) = {
 
     // TODO: get all bags from all lyrics files. Example:
-    val exampleBag = bagOf(readLyrics("src/main/scala/lyricgen/example.txt"), 2)
+    val exampleBag = bagOf(readLyrics("src/main/scala/lyricgen/example.txt", 2), 2)
 
     println(exampleBag.mkString("\n"))
 
@@ -17,10 +17,24 @@ object LyricBags {
     // TODO: Generate the lyrics from this final bag
   }
 
+  def generateLyrics(bag : Map[List[String], List[String]]) = {
+    // Choose one word from <START>, currWord
+
+    // Loop until you reach a word with <END> in value list
+      // Get value list of currWord
+      // Make a random choice from value list, choice
+      // Add currWord to string
+        // Check if word is <N> then add a \n
+        // else add the word as is
+      // set currWord equal to choice
+
+    // return string
+  }
+
   // Function reads in a text file with song lyrics, and cleans the lyrics from empty lines,
   // and ghost lyrics [those parts between ()] for when we are making the bag of words.
   // I/O: String => Array[String]
-  def readLyrics(path : String) : List[String] = {
+  def readLyrics(path : String, N : Int) : List[String] = {
     val l = Source.fromFile(path)
       .getLines
       .toArray
@@ -36,6 +50,7 @@ object LyricBags {
       .toList
 
     // Adding START
+    var nones = (0 to N - 1).toList
     var lyrics = "<START>" :: l
 
     // Adding END to the last line
